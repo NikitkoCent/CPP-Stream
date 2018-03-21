de18372ed03568ec5e96268c6c12b84ed8e55bcf
Nikita Provotorov <nikita.provotorov.work@gmail.com>
2018-03-21 14:50:49 +1000

    Add 'map' visitor

--- include/stream_visitors.h
+++ include/stream_visitors.h
@@ -1,20 +1,27 @@
 #ifndef CPP_STREAM_STREAM_VISITORS_H
 #define CPP_STREAM_STREAM_VISITORS_H
 
+#include "detail/utility.h"
 #include "stream.h"
+#include "visitor.h"    // stream::Visitor
 #include <cstddef>      // ::std::size_t
-#include <functional>   // ::std::ref
+#include <type_traits>  // ::std::remove_reference_t
+#include <utility>      // ::std::move, ::std::forward
+#include <functional>
+#include <vector>
+
 
 namespace stream
 {
     namespace visitors
     {
-        /*auto get(::std::size_t n)
+        auto get(::std::size_t n)
         {
-            return [n](auto &stream) mutable {
-                using type = typename ::std::remove_reference_t<decltype(stream)>::value_type;
+            auto lambda = [n](auto &stream) mutable {
+                using StreamValueType = typename ::std::remove_reference_t<decltype(stream)>::value_type;
+                using Type = StreamValueType;
 
-                ::std::vector<type> result;
+                ::std::vector<Type> result;
                 result.reserve(n);
 
                 auto iter = stream.begin();
@@ -24,29 +31,53 @@ namespace stream
                     --n;
                 }
 
-                return Stream<type>(::std::move(result));
+                return stream::Stream<Type, ::std::vector<Type>>(::std::move(result));
             };
+
+            return stream::Visitor<decltype(lambda)>(::std::move(lambda));
         }
 
         template<typename OStream>
-        auto printTo(OStream &oStream, const char *delim = " ")
+        auto print_to(OStream &oStream, const char *delim = " ")
         {
-            return [&oStream, delim](auto &stream) mutable -> OStream& {
+            auto lambda = [oStream = ::std::ref(oStream), delim](auto &stream) mutable -> OStream& {
                 auto iter = stream.begin();
                 if (iter == stream.end())
                 {
-                    return oStream;
+                    return oStream.get();
                 }
 
-                oStream << *iter;
+                oStream.get() << *iter;
                 for (++iter; iter != stream.end(); ++iter)
                 {
-                    oStream << delim << *iter;
+                    oStream.get() << delim << *iter;
                 }
 
-                return oStream;
+                return oStream.get();
             };
-        }*/
+
+            return stream::Visitor<decltype(lambda)>(::std::move(lambda));
+        }
+
+
+        template<typename Callable>
+        auto map(Callable &&callable)
+        {
+            auto lambda = [f = ::std::forward<Callable>(callable)](auto &stream) mutable {
+                using StreamValueType = typename ::std::remove_reference_t<decltype(stream)>::value_type;
+                using Type = ::std::decay_t<detail::InvokeResultT<decltype(f), StreamValueType>>;
+
+                ::std::vector<Type> result;
+                for (const StreamValueType &value : stream)
+                {
+                    result.emplace_back(f(value));
+                }
+
+                return Stream<Type, ::std::vector<Type>>(::std::move(result));
+            };
+
+            return stream::Visitor<decltype(lambda)>(::std::move(lambda));
+        }
     }
 }
 
