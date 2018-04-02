f58ad66872b62d49066ae019da8d949c529abe09
Nikita Provotorov <nikita.provotorov.work@gmail.com>
2018-04-03 09:10:41 +1000

    Add special structure for testing

---
+++ tests/noisy.h
@@ -0,0 +1,54 @@
+#ifndef CPP_STREAM_TESTS_NOISY_H
+#define CPP_STREAM_TESTS_NOISY_H
+
+#include <iostream>
+
+struct Noisy
+{
+    int value;
+    int copyCount = 0;
+
+
+    Noisy(int value)
+        : value(value)
+    {}
+
+    Noisy(const Noisy &src)
+    {
+        value = src.value;
+        copyCount = src.copyCount + 1;
+
+        std::cout << "NOISY COPY CONSTRUCTOR" << std::endl;
+    }
+
+    Noisy(Noisy &&src)
+    {
+        value = src.value;
+    }
+
+
+    Noisy& operator=(const Noisy &right)
+    {
+        if (this != &right)
+        {
+            value = right.value;
+            copyCount = right.copyCount;
+
+            std::cout << "NOISY COPY ASSIGNMENT" << std::endl;
+        }
+
+        return *this;
+    }
+
+    Noisy& operator=(Noisy &&right)
+    {
+        if (this != &right)
+        {
+            value = right.value;
+        }
+
+        return *this;
+    }
+};
+
+#endif //CPP_STREAM_NOISY_H
