e30d5779b8eb4e475e6751b839f08982e920b3a9
Nikita Provotorov <nikita.provotorov.work@gmail.com>
2020-06-22 01:52:25 +0700

    Revert "attempt to fix MSVS 2017 build."
    This reverts commit 9f38929d6988835d8aa20b9d723fb09c17c526ac.

--- include/stream/stream.h
+++ include/stream/stream.h
@@ -22,189 +22,37 @@ namespace stream
     };
 
 
-    //namespace detail
-    //{
-    //    template<typename... Ts>
-    //    struct CtadDeductorParams {};
-
-    //    template<typename CDP, typename = void>
-    //    struct CtadDeductor;
-
-
-    //    // Container guide for rvalue reference
-    //    template<typename Container>
-    //    struct CtadDeductor<
-    //                         CtadDeductorParams< Container&& >,
-    //                         VoidT< ::std::enable_if_t< ::std::is_rvalue_reference_v<Container&&> >,
-    //                                ::std::enable_if_t< IsContainerV<Container&&> >
-    //                              >
-    //                       >
-    //    {
-    //        using Type = Stream< typename ContainerTraits<Container>::ValueType, ::std::remove_reference_t<Container> >;
-    //    };
-
-    //    // Container guide for ::std::initializer_list
-    //    template<typename T>
-    //    struct CtadDeductor<
-    //                         CtadDeductorParams< ::std::initializer_list<T> >,
-    //                         VoidT<>
-    //                       >
-    //    {
-    //        using Type = Stream<T>;
-    //    };
-
-    //    // Container guide for values pack
-    //    template<typename T, typename... Ts>
-    //    struct CtadDeductor<
-    //                         CtadDeductorParams< T&&, Ts&&... >
-    //                       >
-    //    {
-    //        using Type = Stream< ::std::remove_reference_t<T&&> >;
-    //    };
-
-
-    //    // Container ref guide for non const lref
-    //    template<typename Container>
-    //    struct CtadDeductor<
-    //                         CtadDeductorParams< Container& >,
-    //                         VoidT< ::std::enable_if_t< IsContainerV<Container&> >
-    //                              >
-    //                       >
-    //    {
-    //        using Type = Stream<typename ContainerTraits<Container&>::ValueType, const Container&>;
-    //    };
-
-    //    // Container ref guide for const lref
-    //    template<typename Container>
-    //    struct CtadDeductor<
-    //                         CtadDeductorParams< const Container& >,
-    //                         VoidT< ::std::enable_if_t< IsContainerV<Container&> >
-    //                              >
-    //                       >
-    //    {
-    //        using Type = Stream<typename ContainerTraits<Container&>::ValueType, const Container&>;
-    //    };
-
-    //    // Container ref guide for const rref
-    //    template<typename Container>
-    //    struct CtadDeductor<
-    //                         CtadDeductorParams< const Container&& >,
-    //                         VoidT< ::std::enable_if_t< IsContainerV<Container&&> >
-    //                              >
-    //                       >
-    //    {
-    //        using Type = Stream<typename ContainerTraits<Container&&>::ValueType, const Container&>;
-    //    };
-
-
-    //    // Generator guide
-    //    template<typename Generator>
-    //    struct CtadDeductor<
-    //                         CtadDeductorParams< Generator&& >,
-    //                         VoidT< ::std::enable_if_t< IsGeneratorV<Generator&&> >
-    //                              >
-    //                       >
-    //    {
-    //        using Type = Stream< typename GeneratorTraits<Generator&&>::ValueType, ::std::remove_reference_t<Generator&&> >;
-    //    };
-
-
-    //    // Range guide
-    //    template<typename Iterator1, typename Iterator2>
-    //    struct CtadDeductor<
-    //                         CtadDeductorParams< Iterator1&&, Iterator2&& >,
-    //                         VoidT< ::std::enable_if_t< IsRangeV<Iterator1&&> >
-    //                              >
-    //                       >
-    //    {
-    //        using Type = Stream< typename RangeTraits<Iterator1&&>::ValueType, RemoveCRefT<Iterator1&&> >;
-    //    };
-
-
-    //    template<typename... Ts>
-    //    using CtadDeductorT = typename CtadDeductor< CtadDeductorParams<Ts...> >::Type;
-    //}
-
-
-    // Container guide for rvalue reference
-    template<typename Container,
-             ::std::enable_if_t< ::std::is_rvalue_reference_v<Container&&> >* = nullptr,
-             ::std::enable_if_t< IsContainerV<Container&&> >* = nullptr
-    >
+    // Container guides
+    template<typename Container>
     Stream(Container&&) -> Stream<typename ContainerTraits<Container>::ValueType, ::std::remove_reference_t<Container>>;
 
-    // Container guide for ::std::initializer_list
     template<typename T>
     Stream(::std::initializer_list<T>) -> Stream<T>;
 
-    // Container guide for values pack
     template<typename T, typename... Ts>
     Stream(T&&, Ts&&...) -> Stream<::std::remove_reference_t<T>>;
 
 
-    // Container ref guide for non const lref
-    template<typename Container,
-             ::std::enable_if_t< IsContainerV<Container&> >* = nullptr
-    >
-    Stream(Container&) -> Stream<typename ContainerTraits<Container&>::ValueType, const Container&>;
+    // Container cref guides
+    template<typename Container>
+    Stream(Container&) -> Stream<typename ContainerTraits<Container>::ValueType, const Container&>;
 
-    // Container ref guide for const lref
-    template<typename Container,
-             ::std::enable_if_t< IsContainerV<Container&> >* = nullptr
-    >
-    Stream(const Container&) -> Stream<typename ContainerTraits<Container&>::ValueType, const Container&>;
+    template<typename Container>
+    Stream(const Container&) -> Stream<typename ContainerTraits<Container>::ValueType, const Container&>;
 
-    // Container ref guide for const rref
-    template<typename Container,
-             ::std::enable_if_t< IsContainerV<Container&&> >* = nullptr
-    >
-    Stream(const Container&&) -> Stream<typename ContainerTraits<Container&&>::ValueType, const Container&>;
+    template<typename Container>
+    Stream(const Container&&) -> Stream<typename ContainerTraits<Container>::ValueType, const Container&>;
 
 
-    // Generator guide
-    template<typename Generator,
-             ::std::enable_if_t< IsGeneratorV<Generator&&> >* = nullptr
-    >
-    Stream(Generator&&) -> Stream< typename GeneratorTraits<Generator&&>::ValueType, ::std::remove_reference_t<Generator&&> >;
+    // Generator guides
+    template<typename Generator>
+    Stream(Generator&&) -> Stream<typename GeneratorTraits<Generator>::ValueType, ::std::remove_reference_t<Generator>>;
 
 
-    // Range guide
-    template<typename Iterator1, typename Iterator2,
-             ::std::enable_if_t< IsRangeV<Iterator1&&> >* = nullptr
-    >
-    Stream(Iterator1&&, Iterator2&&) -> Stream< typename RangeTraits<Iterator1&&>::ValueType, RemoveCRefT<Iterator1&&> >;
-
-    //// Container guides
-    //template<typename Container>
-    //Stream(Container&&) -> Stream<typename ContainerTraits<Container>::ValueType, ::std::remove_reference_t<Container>>;
-
-    //template<typename T>
-    //Stream(::std::initializer_list<T>) -> Stream<T>;
-
-    //template<typename T, typename... Ts>
-    //Stream(T&&, Ts&&...) -> Stream<::std::remove_reference_t<T>>;
-
-
-    //// Container ref guides
-    //template<typename Container>
-    //Stream(Container&) -> Stream<typename ContainerTraits<Container>::ValueType, const Container&>;
-
-    //template<typename Container>
-    //Stream(const Container&) -> Stream<typename ContainerTraits<Container>::ValueType, const Container&>;
-
-    //template<typename Container>
-    //Stream(const Container&&) -> Stream<typename ContainerTraits<Container>::ValueType, const Container&>;
-
-
-    //// Generator guides
-    //template<typename Generator>
-    //Stream(Generator&&) -> Stream<typename GeneratorTraits<Generator>::ValueType, ::std::remove_reference_t<Generator>>;
-
-
-    //// Range guides
-    //template<typename Iterator1, typename Iterator2>
-    //Stream(Iterator1&&, Iterator2&&) -> Stream<typename RangeTraits<Iterator1>::ValueType,
-    //                                           RemoveCRefT<Iterator1>>;
+    // Range guides
+    template<typename Iterator1, typename Iterator2>
+    Stream(Iterator1&&, Iterator2&&) -> Stream<typename RangeTraits<Iterator1>::ValueType,
+                                               RemoveCRefT<Iterator1>>;
 }
 
 #endif //CPP_STREAM_STREAM_H
